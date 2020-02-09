e1189d2e49ca4eabd2a502e2204a586ec588b14b
kinako555 <dodohunter_3658@yahoo.co.jp>
2020-02-09 23:19:41 +0900

    サーバーと接続完了?

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { SelectionsComponent } from './selections/selections.component';
+
+import { MyHttpService } from "./servicese/my-http.service";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SelectionsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    MyHttpService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/models/season.ts
@@ -0,0 +1,4 @@
+export class Season {
+	"id"  : string;
+	"name": string;
+}
